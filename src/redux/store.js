53fb5aff29cd43e96eb9f61689dfc4968a1b3572
Guled Mohamed <you@example.com>
2021-06-01 11:50:26 +0300

    removing redux-logger

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,7 +3,7 @@ import logger from 'redux-logger';
 import { persistStore } from 'redux-persist';
 import rootReducer from './rootReducer';
 
-const middilewares = [logger];
+const middilewares = [];
 if (process.env.NODE_ENV === 'development') {
   middilewares.push(logger);
 }
